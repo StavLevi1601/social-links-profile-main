22dc2d4f47e5419cc4dd45d21173ec5a672742cf
Stav Levi <stav.levi@netnut.io>
2024-04-21 00:27:14 +0300

    chage all to Tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -5,124 +5,38 @@ function App() {
     <>
       <div className="bg-black min-h-lvh w-full flex justify-center items-center">
         <div className="bg-[#1F1F1F] m-auto flex flex-col items-center rounded-[12px]">
-          <div
-            style={{
-              display: "flex",
-              flexDirection: "column",
-              gap: "24px",
-              borderRadius: "12px",
-            }}
-          >
-            <div style={{ padding: "40px 148px 24px 148px" }}>
+          <div className="flex flex-col gap-[24px] rounded-[12px]">
+            <div className="pt-[40px] pr-[148px] pb-[24px] pl-[148px] ">
               <img
                 src={image}
                 alt="description of the image"
                 className="rounded-full w-[88px] h-[88px]"
               />
             </div>
-            <div style={{}}>
-              <div
-                style={{
-                  color: "white",
-                  textAlign: "center",
-                }}
-              >
-                Jessica Randall
-              </div>
-              <div
-                style={{
-                  color: "#C4F82A",
-                  textAlign: "center",
-                }}
-              >
+            <div>
+              <div className="text-white text-center">Jessica Randall</div>
+              <div className="text-[#C4F82A] text-center">
                 London, United Kingdom
               </div>
             </div>
-            <div
-              style={{
-                color: "white",
-                textAlign: "center",
-                marginBottom: "24px",
-              }}
-            >
+            <div className="text-white text-center mb-px">
               "Front-end developer and avid reader."
             </div>
           </div>
-          <div
-            style={{
-              flexDirection: "column",
-              gap: "10px",
-              display: "flex",
-            }}
-          >
-            <div
-              style={{
-                backgroundColor: "#333333",
-                color: "white",
-                height: "45px",
-                width: "304px",
-                borderRadius: "8px",
-                textAlign: "center",
-                padding: "12px",
-              }}
-            >
+          <div className="flex flex-col gap-[10px]">
+            <div className="bg-[#333333] text-white h-[45px] w-[304px] rounded-[8px] text-center p-[12px]">
               Github
             </div>
-            <div
-              style={{
-                backgroundColor: "#333333",
-                color: "white",
-
-                height: "45px",
-                width: "304px",
-                borderRadius: "8px",
-                textAlign: "center",
-                padding: "12px",
-              }}
-            >
+            <div className="bg-[#333333] text-white h-[45px] w-[304px] rounded-[8px] text-center p-[12px]">
               Frontend Mentor
             </div>
-            <div
-              style={{
-                backgroundColor: "#333333",
-                color: "white",
-
-                height: "45px",
-                width: "304px",
-                borderRadius: "8px",
-                textAlign: "center",
-                padding: "12px",
-              }}
-            >
+            <div className="bg-[#333333] text-white h-[45px] w-[304px] rounded-[8px] text-center p-[12px]">
               Linkedin
             </div>
-            <div
-              style={{
-                backgroundColor: "#333333",
-                color: "white",
-
-                height: "45px",
-                width: "304px",
-                borderRadius: "8px",
-                textAlign: "center",
-                padding: "12px",
-              }}
-            >
+            <div className="bg-[#333333] text-white h-[45px] w-[304px] rounded-[8px] text-center p-[12px]">
               Twitter
             </div>
-            <div
-              style={{
-                backgroundColor: "#333333",
-                color: "white",
-
-                height: "45px",
-                width: "304px",
-                borderRadius: "8px",
-                textAlign: "center",
-                padding: "12px",
-                marginBottom: "40px",
-              }}
-            >
+            <div className="bg-[#333333] text-white h-[45px] w-[304px] rounded-[8px] text-center p-[12px] mb-[40px]">
               Instagram
             </div>
           </div>
